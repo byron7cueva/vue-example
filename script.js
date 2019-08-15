efdc3ea49bea04c32c25d534d972ab2127a2496a
Byron Cueva <byron7cueva@gmail.com>
2019-08-15 06:32:02 -0500

    Primer ejemplo

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.esm.browser.js'
+//import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.esm.browser.js'
 
 let app = new Vue({
     el: '#app',
     data: {
-        nombre: "Byron"
+        nombre: "Byron",
+        url: "https://google.com",
+        pagina: "Ir a google"
     }
 })
